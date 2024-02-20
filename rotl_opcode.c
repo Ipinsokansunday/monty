0b4c28c40ccf2f53a644aee4b6bd98aaa860c11d
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-02-20 04:02:24 -0800

    0x01. AirBnB clone - Web static

--- rotl_opcode.c
+++ rotl_opcode.c
@@ -9,13 +9,14 @@
 void f_rotl(stack_t **stack_head, __attribute__((unused)) unsigned int num_count)
 {
 	stack_t *temp = *stack_head;
-        stack_t *aux = (*stack_head)->next;
-	
+	stack_t *aux = NULL;
+
 	if (*stack_head == NULL || (*stack_head)->next == NULL)
 	{
 		return;
 	}
 
+	aux = (*stack_head)->next;
 	aux->prev = NULL;
 
 	while (temp->next != NULL)
